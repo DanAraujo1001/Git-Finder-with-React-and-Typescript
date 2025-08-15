c54efa732c2d8c14da0f37b914efd42c7b3f9c95
Danilo Araujo <danilocostafp@gmail.com>
2025-08-15 14:22:14 -0300

    chore: configure react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css'
+import { Outlet } from "react-router-dom";
+import "./App.css";
 
 function App() {
-
   return (
     <div>
       <h1>GitHub Finder</h1>
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
