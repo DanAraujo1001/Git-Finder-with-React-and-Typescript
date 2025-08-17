cd205e6942bbe7b05f3f179fb24eb0d4dfb5f50a
Danilo Araujo <danilocostafp@gmail.com>
2025-08-16 21:08:38 -0300

    style: add styling for app and Search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
+import classes from "./Search.module.css";
 
 interface SearchProps {
   loadUser: (userName: string) => Promise<void>;
@@ -9,17 +10,17 @@ const Seacrh = ({ loadUser }: SearchProps) => {
   const [userName, setUserName] = useState("");
 
   return (
-    <div>
+    <div className={classes.search}>
       <h2>Busque por um usuário</h2>
       <p>Conheça seus melhores repositórios</p>
-      <div>
+      <div className={classes.search_container}>
         <input
           type="text"
           placeholder="Digite o nome do usuário"
           onChange={(e) => setUserName(e.target.value)}
         />
         {/* Eu não entendi direito essa função */}
-        <button onClick={() => loadUser(userName)}> 
+        <button onClick={() => loadUser(userName)}>
           <BsSearch />
         </button>
       </div>
